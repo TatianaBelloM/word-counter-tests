50a2d92f45c11c5893af4fe5f5d2222ac31ac3cb
TatianaBelloM <ttattiana_bemo@hotmail.com>
2026-06-29 19:36:39 -0500

    fixed bug wait result

--- src/test/java/com/lumu/qa/stepdefinitions/RepeatedWordsStepsDefinitions.java
+++ src/test/java/com/lumu/qa/stepdefinitions/RepeatedWordsStepsDefinitions.java
@@ -3,10 +3,14 @@ package com.lumu.qa.stepdefinitions;
 import io.cucumber.java.en.Then;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.ensure.Ensure;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import com.lumu.qa.screenplay.config.Timeouts;
 import com.lumu.qa.screenplay.questions.KeywordDensityCount;
 import com.lumu.qa.screenplay.questions.TopKeywordCount;
 import com.lumu.qa.screenplay.questions.TopKeywordWord;
+import com.lumu.qa.screenplay.ui.WordCounterPage;
 
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class RepeatedWordsStepsDefinitions {
 
@@ -20,6 +24,8 @@ public class RepeatedWordsStepsDefinitions {
     @Then("the top {int} keyword should be {string} with count {int}")
     public void theTopKeywordShouldBeWithCount(int position, String word, int count) {
         OnStage.theActorInTheSpotlight().attemptsTo(
+                WaitUntil.the(WordCounterPage.keywordWordAt(position), isVisible())
+                        .forNoMoreThan(Timeouts.ELEMENT_VISIBILITY_SECONDS).seconds(),
                 Ensure.that(TopKeywordWord.atPosition(position)).isEqualTo(word),
                 Ensure.that(TopKeywordCount.atPosition(position)).isEqualTo(count)
         );
---
+++ src/test/java/com/lumu/qa/stepdefinitions/ScenarioSetup.java
@@ -0,0 +1,13 @@
+package com.lumu.qa.stepdefinitions;
+
+import io.cucumber.java.Before;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+public class ScenarioSetup {
+
+    @Before
+    public void setTheStage() {
+        OnStage.setTheStage(new OnlineCast());
+    }
+}
